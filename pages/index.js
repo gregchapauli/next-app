7915cc3c0642c697d7532807a020b9ec3042e45d
Gregory Caloin <gregchapauli@gmail.com>
2022-04-14 09:15:10 +0200

    Création de la fonction getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,17 @@
 import Head from "next/head";
+import Link from "next/link";
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
-  const [posts, setPosts] = useState([]);
+export default function Home({ posts }) {
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
-    const data = fetch("https://jsonplaceholder.typicode.com/posts?_limit=4")
-      .then((r) => r.json())
-      .then(setPosts);
+    const timer = setInterval(() => setCount((n) => n + 1), 1000);
+    return () => {
+      clearInterval(timer);
+    };
   }, []);
 
   return (
@@ -17,13 +19,31 @@ export default function Home() {
       <Head>
         <title>Mon super blog</title>
       </Head>
+      <h1>Cout: {count}</h1>
       <ul>
         {posts.map((post) => (
           <li>
-            <h3>{post.title}</h3>
+            <Link href={`/blog/${post.id}`}>
+              <a>
+                <h3>
+                  {post.id} - {post.title}
+                </h3>
+              </a>
+            </Link>
           </li>
         ))}
       </ul>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const posts = await fetch(
+    "https://jsonplaceholder.typicode.com/posts?_limit=4"
+  ).then((r) => r.json());
+  return {
+    props: {
+      posts,
+    },
+  };
 }
